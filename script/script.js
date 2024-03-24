058216d0590b8f1d7b56f1e1603b0ea0a3a3feab
Mathias Cabrera <mathiasdesign1@gmail.com>
2024-03-24 13:51:49 -0300

    Color changes of the hamburger icon when scrolling. Close menu option added.

--- script/script.js
+++ script/script.js
@@ -1,43 +1,88 @@
 //nav-bar scroll
-window.addEventListener("scroll",function(){
-    let navBar = this.document.getElementById("navbar");
-    let iconNavBar = this.document.getElementById("iconNav");
+let menu_visible = false;
+let menu = document.getElementById("nav");
+let iconHamburger = document.getElementById("iconNav");
+let iconX = document.getElementById("iconNavX");
+let containIcon = document.getElementById("navbar");
 
-    if(this.window.scrollY>=310){
-        navBar.style.border = "0.2rem solid rgba(195, 195, 195,0.4)";
-        navBar.style.backgroundColor = "rgba(0, 3, 9,0.6)";
-        navBar.style.boxShadow = "0 0.8rem 3.2rem 0 rgba(0, 3, 9, *)";
-        iconNavBar.style.color= "#c3c3c3";
+document.addEventListener('DOMContentLoaded', function() {
+    window.addEventListener('scroll', function() {
+      let scrollPosition = (window.scrollY - 370);
+
+      let sectionPresentation = document.getElementById('presentation').offsetTop;
+      let sectionWorkExperience = document.getElementById('workexperience').offsetTop;
+      let sectionCoursesTaken = document.getElementById('coursestaken').offsetTop;
+      let sectionProjects = document.getElementById('myprojects').offsetTop;
+      let sectionTechnologies = document.getElementById('technologies').offsetTop;
+      let sectionFooter = document.getElementById('footer').offsetTop;
+        
+    if (scrollPosition > sectionPresentation) {
+        containIcon.style.border = "0.2rem solid rgba(195, 195, 195,0.4)";
+        containIcon.style.backgroundColor = "rgba(7, 8, 10,0.8)";
+        containIcon.style.boxShadow = "0 0.8rem 3.2rem 0 rgba(0, 3, 9, *)";
+        iconHamburger.style.color= "#f4f4f6";
     }
-    if(this.window.scrollY>=1850){
-        navBar.style.border = "0.2rem solid rgba(195, 195, 195,0.4)";
-        navBar.style.boxShadow = "0 0.8rem 3.2rem 0 rgba(0, 3, 9, 0)";
-        iconNavBar.style.color= "#1b1b1b";
+    if (scrollPosition > sectionProjects) {
+        containIcon.style.border = "0.2rem solid rgba(0, 3, 9,0.4)";
+        containIcon.style.backgroundColor = "rgba(244, 244, 246, 0.8)";
+        containIcon.style.boxShadow = "0 0.8rem 3.2rem 0 rgba(0, 3, 9, 0.4)";
+        iconHamburger.style.color= "#000309";
     }
-    if(this.window.scrollY<310){
-        navBar.style.border = "0.2rem solid rgba(0, 3, 9,0.4)";
-        navBar.style.backgroundColor = "rgba(244, 244, 246,0.8)";
-        navBar.style.boxShadow = "0 0.8rem 3.2rem 0 rgba(0, 3, 9, 0.4)";
-        iconNavBar.style.color= "#000309";
+    if (scrollPosition > sectionTechnologies) {
+        containIcon.style.border = "0.2rem solid rgba(0, 3, 9,0.4)";
+        containIcon.style.backgroundColor = "rgba(255, 255, 252, 0.8)";
+        containIcon.style.boxShadow = "0 0.8rem 3.2rem 0 rgba(0, 3, 9, 0.4)";
+        iconHamburger.style.color= "#000309";
     }
-})
+    if (scrollPosition > (sectionFooter-450)) {
+        containIcon.style.border = "0.2rem solid rgba(195, 195, 195,0.4)";
+        containIcon.style.backgroundColor = "rgba(7, 8, 10,0.8)";
+        containIcon.style.boxShadow = "0 0.8rem 3.2rem 0 rgba(0, 3, 9, *)";
+        iconHamburger.style.color= "#f4f4f6";
+    }
+    if (scrollPosition < sectionPresentation) {
+        containIcon.style.border = "0.2rem solid rgba(0, 3, 9,0.4)";
+        containIcon.style.backgroundColor = "rgba(244, 244, 246,0.8)";
+        containIcon.style.boxShadow = "0 0.8rem 3.2rem 0 rgba(0, 3, 9, 0.4)";
+        iconHamburger.style.color= "#000309";
+    }
+    })});
 
 //menu visibility
-var menu_visible = false;
-var menu = document.getElementById("nav");
-
 function showHideMenu(){
     if(menu_visible == false){
-        menu.style.display="flex";
-        menu.style.justifyContent="center";
-        menu.style.placeItems="center";
+        iconHamburger.style.display="none";
+        iconX.style.display="block";
+        menu.style.display="block";
         menu.style.zIndex=100;
+        containIcon.style.backgroundColor= "transparent";
+        containIcon.style.backdropFilter="none";
+        containIcon.style.border="0";
+        containIcon.style.boxShadow= "0 0 0 0";
+        containIcon.style.borderRadius="0";
+        
+        menu.addEventListener('click', function() {
+            menu.style.display = 'none';
+            iconHamburger.style.display="block";
+            iconX.style.display="none";
+            menu.style.display="none";
+            containIcon.style.backdropFilter="blur(1px)";
+            containIcon.style.border="0.2rem solid rgba(0, 3, 9,0.4)";
+            containIcon.style.boxShadow= "0 0.8rem 3.2rem 0 rgba(0, 3, 9, 0.1)";
+            containIcon.style.borderRadius="50%";
+            menu_visible = false;
+        });
         menu_visible = true;
     }
     else{
+        iconHamburger.style.display="block";
+        iconX.style.display="none";
         menu.style.display="none";
+        containIcon.style.backdropFilter="blur(1px)";
+        containIcon.style.border="0.2rem solid rgba(0, 3, 9,0.4)";
+        containIcon.style.boxShadow= "0 0.8rem 3.2rem 0 rgba(0, 3, 9, 0.1)";
+        containIcon.style.borderRadius="50%";
         menu_visible = false;
-
     }
 }
 
@@ -87,7 +132,7 @@ function moveToLeft(){
 
 //Carousel Technology
 //infinite horizontal scroll
-const scrollers = document.querySelector(".scroller");
+const scrollers = document.querySelectorAll(".scroller");
 
 // If a user hasn't opted in for recuded motion, then we add the animation
 if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
